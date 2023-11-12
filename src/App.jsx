ca8198f47adf05caeff8b54ec954bbb6db7aa704
ONyeNYC <dis2isaac@yahoo.com>
2023-11-12 08:52:02 -0500

    Stop Watch Completed

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import Stopwatch from "./Stopwatch";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <Stopwatch />;
+  }
+}
+
+export default App;
